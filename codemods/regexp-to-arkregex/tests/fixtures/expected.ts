8dde04548848db7f06a8e3aa441ffff36defeaac
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-10-28 22:32:55 +0330

    fix: fix tests

---
+++ codemods/regexp-to-arkregex/tests/fixtures/expected.ts
@@ -0,0 +1,3 @@
+import { regex } from "arkregex";
+const pattern = regex("\\d+", "g");
+const emailRegex = regex("^[a-z]+@[a-z]+\\.[a-z]+$");
---
+++ codemods/regexp-to-arkregex/tests/fixtures/input.ts
@@ -0,0 +1,2 @@
+const pattern = new RegExp("\\d+", "g");
+const emailRegex = new RegExp("^[a-z]+@[a-z]+\\.[a-z]+$");
